d62270f466a75f25cf86e0e775596f4d3c291444
Orion138 <nathan.boosinger@gmail.com>
2016-02-23 16:01:06 -0600

    Added app icons

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment this line to define a global platform for your project
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.0'
+use_frameworks!
+
+target 'showcase' do
+pod 'Firebase', '>= 2.5.0'
+pod 'Alamofire', '~> 3.0'
+end
+
---
+++ showcase/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  showcase
+//
+//  Created by Nathan Boosinger on 2/18/16.
+//  Copyright © 2016 Nathan Boosinger. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
+
+//Keys
+let KEY_UID = "uid"
+
+//Segues
+let SEGUE_LOGGED_IN = "loggedIn"
+
+//Status codes
+let STATUS_ACCOUNT_NONEXIST = -8
